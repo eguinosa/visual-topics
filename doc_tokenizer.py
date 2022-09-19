b7e6e033b4a29c249620288ef5f5c203128b1fd5
Gelin Eguinosa <eguinosa@gmail.com>
2022-09-18 20:24:58 -0400

    Finish Doc's Vocabulary tokenizer.

--- doc_tokenizer.py
+++ doc_tokenizer.py
@@ -96,7 +96,6 @@ class DocTokenizer:
         Args:
             doc: String with the document from which we are going to create the
                 vocabulary.
-
         Returns:
             Set(str) containing the words in the vocabulary of the document.
         """
@@ -108,26 +107,96 @@ class DocTokenizer:
         doc_tokens = self.nlp(doc)
         # Create vocabulary.
         doc_vocab = {
-            token_word(token) for token in doc_tokens if is_acceptable(token)
+            token_word(token) for token in doc_tokens if vocab_acceptable(token)
         }
         # The words in the vocabulary of the document.
         return doc_vocab
 
+    def docs_vocab_tokenizer(self, docs: iter):
+        """
+        Tokenize an iterable of documents creating the vocabulary for each of
+        the documents and returning them as a List[set], with each set()
+        containing the vocabulary of each document.
+
+        We use nlp.pipe(texts) to speed up the process of tokenization.
+
+        Args:
+            docs: Iterable with the documents we have to tokenize.
+        Returns:
+            List[set] with the vocabulary of the documents.
+        """
+        # Create the default list of sets.
+        docs_vocab_list = []
+
+        # Use nlp.pipe to tokenize the documents.
+        for doc_tokens in self.nlp.pipe(docs):
+            doc_vocab = {
+                token_word(token) for token in doc_tokens
+                if vocab_acceptable(token)
+            }
+            # Add the vocabulary of the document to the list.
+            docs_vocab_list.append(doc_vocab)
+
+        # The Vocabularies of the documents.
+        return docs_vocab_list
+
 
 def token_word(token: Token):
     """
-    Get the best string representation for 'token' in the document.
+    Given 'token' get the string representation most useful for the purpose of
+    the tokenization, usually the word's lemma.
+
+    Args:
+        token: Token from where we are going to get the word representation.
+    Returns:
+        String with the corresponding text representation of the 'token'.
     """
-    # return token.lemma_.lower()
-    pass
+    # Get the Lemma of the word.
+    final_word = token.lemma_.lower().strip()
+    # Check if the word is Covid-19 or Covid-19 related.
+    if final_word == 'covid19':
+        final_word = 'covid-19'
+    elif 'covid19' in final_word and 'covid19' != final_word:
+        final_word = 'covid-19-related'
+    elif 'covid-19' in final_word and 'covid-19' != final_word:
+        final_word = 'covid-19-related'
+    return final_word
 
 
-def is_acceptable(token: Token):
+def vocab_acceptable(token: Token):
     """
-    Check if a given token is acceptable as a word of the vocabulary in the
-    corpus.
+    Check if a given token is a word, has 2 or more character, or is
+    alphanumeric starting with a character.
+
+    Args:
+        token: Token that we are going analyze to see if it's appropriate for
+            the vocabulary of the document.
+    Returns:
+        Bool indicating if the Token can be part of the document's vocabulary or
+            not.
     """
-    pass
+    token_text = token.lower_
+    # No - if token length is too short or too long.
+    if len(token_text) < 2 or len(token_text) > 15:
+        return False
+    # No - if the token represents a number.
+    if token.like_num:
+        return False
+    # No - if the token is a Stop-word.
+    if token.is_stop:
+        return False
+    # Ok - if the word is all alphabetic.
+    if token.is_alpha:
+        return True
+    # Ok - if token has covid-19 in it.
+    if 'covid19' in token_text or 'covid-19' in token_text:
+        return True
+    # Ok - if it has hyphens (-) and only alphabetic words.
+    hyphenated_words = token_text.split('-')
+    if all(map(lambda x: x.isalpha() or x in {'covid', '19', 'covid19'}, hyphenated_words)):
+        return True
+    # No by default - if none of the above cases apply.
+    return False
 
 
 if __name__ == '__main__':
