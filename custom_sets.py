e7e2b5e57e0e24f4c43bcd3e29228b009a160859
Gelin Eguinosa <eguinosa@gmail.com>
2022-09-19 19:50:58 -0400

    Add Custom Set of incorrect acronyms in the Cord-19 dataset.

---
+++ custom_sets.py
@@ -0,0 +1,21 @@
+# Gelin Eguinosa Rosique
+# 2022
+
+wrong_acronyms = {
+    'ABSTRACT': 'abstract',
+    'BACKGROUND': 'background',
+    'CONCLUSION': 'conclusion',
+    'CONCLUSIONS': 'conclusion',
+    'DESIGN': 'design',
+    'HYPOTHESIS': 'hypothesis',
+    'INTRODUCTION': 'introduction',
+    'MATERIAL': 'material',
+    'MATERIALS': 'material',
+    'METHOD': 'method',
+    'METHODS': 'method',
+    'OBJECTIVE': 'objective',
+    'PARTICIPANT': 'participant',
+    'PARTICIPANTS': 'participant',
+    'RESULT': 'result',
+    'RESULTS': 'result',
+}
