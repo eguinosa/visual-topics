96b0f598c69fd24bc371f71ae4b84ca79e273cc9
Gelin Eguinosa <eguinosa@gmail.com>
2022-09-20 03:00:17 -0400

    Add method to find the closest vector.

--- util_funcs.py
+++ util_funcs.py
@@ -21,3 +21,38 @@ def cos_sim(a: np.ndarray, b: np.ndarray):
     # Transform from float32 to float (float32 is not JSON serializable)
     result = float(result)
     return result
+
+
+def closest_vector(embedding, vectors_dict: dict):
+    """
+    Given the embedding of a document or word and a dictionary containing a
+    group of IDs with their embeddings. Find the closest vector to the given
+    embedding using cosine similarity.
+
+    Args:
+        embedding: Numpy.ndarray with the embedding of the word or document that
+            we are using to find the closest vector.
+        vectors_dict: Dictionary containing the vectors with their IDs as
+            keys and their embeddings as values.
+
+    Returns:
+        A tuple with the ID of the closest vector and its similarity to the
+            'embedding' we received as parameter.
+    """
+    # Use iter to get the vectors IDs and their embeddings.
+    vector_iter = iter(vectors_dict.items())
+
+    # Get cosine similarity to the first vector.
+    closest_vector_id, vector_embed = next(vector_iter)
+    max_similarity = cos_sim(embedding, vector_embed)
+
+    # Iterate through the rest of the vectors.
+    for vector_id, vector_embed in vector_iter:
+        new_similarity = cos_sim(embedding, vector_embed)
+        if new_similarity > max_similarity:
+            # New Closer Vector
+            closest_vector_id = vector_id
+            max_similarity = new_similarity
+
+    # The closest vector ID with its similarity to the 'embedding'.
+    return closest_vector_id, max_similarity
