045e0c556e64e3d9294529257daa48b1983c0745
Gelin Eguinosa <eguinosa@gmail.com>
2022-09-25 16:31:33 -0400

    Display also the Info about the Mix Topic Models.

--- main_terminal.py
+++ main_terminal.py
@@ -3,6 +3,7 @@
 
 import sys
 from mono_topics import MonoTopics
+from mix_topics import MixTopics
 from time_keeper import TimeKeeper
 from extra_funcs import big_number
 
@@ -13,16 +14,30 @@ if __name__ == '__main__':
     # Terminal Parameters.
     args = sys.argv
 
-    # Load the Available Topic Models.
+    # Display the Available Mono Topic Models.
     model_ids = MonoTopics.saved_models()
-    print("\nAvailable Topic Models:")
+    print("\n<<- Available Mono Topic Models ->>")
     for model_id in model_ids:
-        print(f"\nTopic Model <{model_id}>:")
+        print(f"\nMono Topic Model <{model_id}>:")
         basic_info = MonoTopics.basic_info(model_id)
-        # Display Basic Info of the Model.
+        # Show the Basic Info of the Model.
         print("  Topics:", big_number(basic_info['topic_size']))
         print("  Corpus Size:", big_number(basic_info['corpus_size']))
         print("  Document Model:", basic_info['text_model_name'].replace('_', ' ').title())
+        hierarchy = 'Yes' if basic_info['has_reduced_topics'] else 'No'
+        print("  Topic Hierarchy:", hierarchy)
+
+    # Display the Available Mix Topic Models.
+    model_ids = MixTopics.saved_models()
+    print("\n<<- Available Mix Topic Models ->>")
+    for model_id in model_ids:
+        print(f"\nMix Topic Model <{model_id}>:")
+        basic_info = MixTopics.basic_info(model_id)
+        # Show the basic Info of the Model.
+        print("  Topics:", big_number(basic_info['topic_size']))
+        print("  Corpus Size:", big_number(basic_info['corpus_size']))
+        print("  Document Model: Specter")
+        print("  Vocabulary Model:", basic_info['vocab_model_name'].replace('_', ' ').title())
         hierarchy = 'Yes' if basic_info['has_reduced_topics'] else 'No'
         print("  Topic Hierarchy:", hierarchy)
 
