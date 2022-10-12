3fbe07edbb5ba1cd9487a1cba73c3143430c874e
Gelin Eguinosa <eguinosa@gmail.com>
2022-10-12 19:48:46 -0400

    Support showing the topic's vocabulary in different styles.

--- qvocab_dialog.py
+++ qvocab_dialog.py
@@ -1,14 +1,26 @@
 # Gelin Eguinosa Rosique
 # 2022
-
+import json
 import sys
+from os import mkdir
+from os.path import isdir, isfile, join
 from PyQt6.QtWidgets import (
     QApplication, QDialog, QLabel, QTextEdit, QPushButton, QVBoxLayout,
+    QCheckBox,
 )
 from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QCloseEvent
 
 
 class QVocabDialog(QDialog):
+    """
+    Class to show in a Dialog the words that best describe the topic of a Topic
+    Model.
+    """
+    # Class Data Locations.
+    data_folder = 'project_data'
+    class_folder = 'qvocab_dialog_files'
+    words_only_file = 'show_words_only.json'
 
     def __init__(self, topic_id: str, word_list: list):
         """
@@ -18,9 +30,29 @@ class QVocabDialog(QDialog):
         super().__init__()
         self.setModal(False)
 
+        # Check if the Class Folder exists.
+        if not isdir(self.data_folder):
+            mkdir(self.data_folder)
+        class_folder_path = join(self.data_folder, self.class_folder)
+        if not isdir(class_folder_path):
+            mkdir(class_folder_path)
+
+        # See if we can load the file with the 'words_only' information.
+        words_only_path = join(class_folder_path, self.words_only_file)
+        if not isfile(words_only_path):
+            words_only = False
+        else:
+            # Load file and save its value.
+            with open(words_only_path, 'r') as f:
+                words_only = json.load(f)
+
         # Save the class attributes.
         self.topic_id = topic_id
         self.word_list = word_list
+        self.words_only = words_only
+
+        # Widget used to display the text with the vocabulary of the Topic.
+        self.vocab_text_widget = None
 
         # Create the UI
         self.initializeUI()
@@ -40,26 +72,21 @@ class QVocabDialog(QDialog):
         # Text for the Header.
         word_count = len(self.word_list)
         header_text = f"""
-            <p align='left'><font face='Times New Roman' size='+1'><u>
+            <p align='left'><font face='Times New Roman' size='+1'>
             {word_count} Top Words from {self.topic_id}:
-            </u></font></p>
+            </font></p>
         """
-        # Text for Vocabulary.
-        vocab_text = ''
-        is_first = True
-        for word, sim in self.word_list:
-            # # Check if this is the first element.
-            if not is_first:
-                vocab_text += '\n\n'
-            vocab_text += f"""<p>({word}, {round(sim, 5)})</p>"""
-            # Flag the rest of the elements.
-            if is_first:
-                is_first = False
-
         # Create Labels, Widget & Areas.
         header_label = QLabel(header_text)
-        vocab_read_only = QTextEdit(vocab_text)
+        vocab_read_only = QTextEdit()
         vocab_read_only.setReadOnly(True)
+        self.vocab_text_widget = vocab_read_only
+        self.updateVocabText()
+        words_checkbox = QCheckBox("Words Only")
+        words_checkbox.setChecked(self.words_only)
+        words_checkbox.toggled.connect(
+            lambda checked: self.wordsOnlyChange(checked=checked)
+        )
         done_button = QPushButton('Done')
         # noinspection PyTypeChecker
         done_button.clicked.connect(self.close)
@@ -68,8 +95,62 @@ class QVocabDialog(QDialog):
         main_v_box = QVBoxLayout()
         main_v_box.addWidget(header_label)
         main_v_box.addWidget(vocab_read_only)
+        main_v_box.addWidget(words_checkbox)
         main_v_box.addWidget(done_button, 0, Qt.AlignmentFlag.AlignCenter)
         self.setLayout(main_v_box)
+
+    def updateVocabText(self):
+        """
+        Create or Update the Text Displaying the Vocabulary of the Topic,
+        depending on the value of the 'words_only' attribute.
+        """
+        # Create Text.
+        vocab_text = ''
+        is_first = True
+        for word, sim in self.word_list:
+            # Add a separator for all non-first elements.
+            if not is_first:
+                # Use commas when displaying only words.
+                if self.words_only:
+                    vocab_text += ', '
+                # Separate by lines in other cases.
+                else:
+                    vocab_text += '\n'
+            # Add the Word.
+            if self.words_only:
+                vocab_text += word
+            else:
+                vocab_text += f"""<p>({word}, {round(sim, 5)})</p>"""
+            # Flag the elements after the first.
+            if is_first:
+                is_first = False
+
+        # Change the Text of the Widget.
+        self.vocab_text_widget.setText(vocab_text)
+
+    def wordsOnlyChange(self, checked: bool):
+        """
+        Update the way the words in the vocabulary of the Topic are displayed,
+        and the attribute that stores this state.
+        """
+        # Update Words Only of class attribute.
+        self.words_only = checked
+        # Update Vocabulary Text.
+        self.updateVocabText()
+
+    def closeEvent(self, event: QCloseEvent):
+        """
+        Reimplement the closing event to save the 'words_only' variable so we
+        have the same behavior the next time we open the dialog.
+        """
+        # Saving the value of the Words Only variable.
+        class_folder_path = join(self.data_folder, self.class_folder)
+        words_only_path = join(class_folder_path, self.words_only_file)
+        with open(words_only_path, 'w') as f:
+            json.dump(self.words_only, f)
+
+        # Accept the Closing Event.
+        event.accept()
 
 
 if __name__ == '__main__':
