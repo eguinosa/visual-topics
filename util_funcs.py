892f75a2b5693f18a53c0528d8521e70256c2ab4
Gelin Eguinosa <eguinosa@gmail.com>
2022-10-30 00:22:03 -0400

    Add function to find the average similarity in a group of vectors.

--- util_funcs.py
+++ util_funcs.py
@@ -1,17 +1,24 @@
 # Gelin Eguinosa Rosique
 # 2022
 
+import multiprocessing
 import numpy as np
 from numpy.linalg import norm
 from bisect import bisect
 
-from extra_funcs import progress_bar
+from extra_funcs import progress_bar, progress_msg
 
 # Test imports.
 import sys
-import random
-from pprint import pprint
+# import random
+# from pprint import pprint
 from time_keeper import TimeKeeper
+
+
+# The Core Multiplier to calculate the Chunk sizes when doing Parallelism.
+PEAK_ITERATIONS = 3_750_000  # 150 topics * 25,000 docs
+BASE_ITERATIONS = 925_000  # 37 topics * 25,000 docs
+MAX_CORES = 8
 
 
 def dict_ndarray2list(embeds_dict: dict, show_progress=False):
@@ -190,22 +197,167 @@ def find_top_n(id_values: iter, n=50, top_max=True, iter_len=-1, show_progress=F
     return top_ids_values
 
 
+def mean_similarity(vectors_ndarray: list, parallelism=False, show_progress=False):
+    """
+    Find the average similarity between the vectors inside the given vector
+    cluster/group 'vectors_ndarray'.
+
+    Args:
+        vectors_ndarray: List[Numpy.ndarray] with the vector inside the cluster.
+        parallelism: Bool indicating if we can use multiprocessing to speed up
+            the execution of the program.
+        show_progress: A Bool representing whether we show the progress of
+            the method or not.
+    Returns:
+        Float between 0.0 and 1.0 with the average similarity between the
+            vectors.
+    """
+    # Check we have enough vectors.
+    if len(vectors_ndarray) < 2:
+        return 1.0
+
+    # See if we can use parallelism.
+    total_iterations = (len(vectors_ndarray) * (len(vectors_ndarray) - 1)) // 2
+    if parallelism and total_iterations > BASE_ITERATIONS:
+        return mean_similarity_parallel(vectors_ndarray, show_progress)
+
+    # Progress Variables.
+    # Add the action of calculating the average calculation to total.
+    count = 0
+    total = total_iterations + 1
+
+    # Simpler code if we don't have to show the progress.
+    if show_progress:
+        # Get the Similarities between the vectors.
+        vector_similarities = []
+        for i in range(1, len(vectors_ndarray)):
+            for j in range(i):
+                # Calculate Similarity.
+                new_sim = cosine_sim(vectors_ndarray[i], vectors_ndarray[j])
+                vector_similarities.append(new_sim)
+                # Progress.
+                count += 1
+                progress_bar(count, total)
+    else:
+        # Create list with the similarity between the vectors.
+        vector_similarities = [
+            cosine_sim(vectors_ndarray[i], vectors_ndarray[j])
+            for i in range(1, len(vectors_ndarray))
+            for j in range(i)
+        ]
+    # Calculate the Average.
+    average_sim = sum(vector_similarities) / len(vector_similarities)
+    if show_progress:
+        count += 1
+        progress_bar(count, total)
+    # Average of the Similarities.
+    return average_sim
+
+
+def mean_similarity_parallel(vectors_ndarray: list, show_progress=False):
+    """
+    Parallelism version of the mean_similarity() method.
+
+    Find the average similarity between the vectors inside the given vector
+    cluster/group 'vectors_ndarray'.
+
+    Args:
+        vectors_ndarray: List[Numpy.ndarray] with the vector inside the cluster.
+        show_progress: A Bool representing whether we show the progress of
+            the method or not.
+    Returns:
+        Float between 0.0 and 1.0 with the average similarity between the
+            vectors.
+    """
+    # Determine the number of cores to be used. (I made my own formula)
+    total_iterations = (len(vectors_ndarray) * (len(vectors_ndarray) - 1)) // 2
+    optimal_cores = min(multiprocessing.cpu_count(), MAX_CORES)
+    efficiency_mult = min(float(1), total_iterations / PEAK_ITERATIONS)
+    core_count = max(2, round(efficiency_mult * optimal_cores))
+    # Create chunk size to process the tasks in the cores.
+    chunk_size = max(1, total_iterations // 100)
+
+    # Create Tuple Parameters.
+    tuple_params = [
+        (vectors_ndarray[i], vectors_ndarray[j])
+        for i in range(1, len(vectors_ndarray))
+        for j in range(i)
+    ]
+    # Progress Variables.
+    # Add the action of calculating the average to total.
+    count = 0
+    total = total_iterations + 1
+
+    # Calculate the Similarities between the vectors using Parallelism.
+    with multiprocessing.Pool(processes=core_count) as pool:
+        # Use Pool.imap() if we have to show the progress.
+        if show_progress:
+            # Report Parallelization.
+            progress_msg(f"Using Parallelization <{core_count} cores>")
+            # Create Lazy iterator of results using Pool.imap()
+            imap_results_iter = pool.imap(
+                _custom_cosine_sim, tuple_params, chunksize=chunk_size
+            )
+            # Add the Similarities between the vectors to the list.
+            vector_similarities = []
+            for new_sim in imap_results_iter:
+                # Add Similarity to list.
+                vector_similarities.append(new_sim)
+                # Progress.
+                count += 1
+                progress_bar(count, total)
+        # No need to show progress, use Pool.map()
+        else:
+            # Create List with the similarities.
+            vector_similarities = pool.map(
+                _custom_cosine_sim, tuple_params, chunksize=chunk_size
+            )
+    # Calculate the Average.
+    average_sim = sum(vector_similarities) / len(vector_similarities)
+    if show_progress:
+        count += 1
+        progress_bar(count, total)
+    # Average of the Similarities.
+    return average_sim
+
+
+def _custom_cosine_sim(vectors_tuple: tuple):
+    """
+    Custom-made version of the method cosine_sim() to use with parallelization.
+    """
+    embed_a, embed_b = vectors_tuple
+    return cosine_sim(embed_a, embed_b)
+
+
 if __name__ == '__main__':
     # Record Program Runtime.
     _stopwatch = TimeKeeper()
     # Terminal Arguments.
     _args = sys.argv
 
-    # Create a list of tuples to get the Top N elements.
-    _origin = random.sample(range(12, 100), 20)
-    _tuples = [(n, n) for n in _origin]
-    print("\nElements to Sort:")
-    pprint(_tuples)
+    # # Create a list of tuples to get the Top N elements.
+    # _origin = random.sample(range(12, 100), 20)
+    # _tuples = [(n, n) for n in _origin]
+    # print("\nElements to Sort:")
+    # pprint(_tuples)
+    # # --------------------------------------------------
+    # # Show Top Elements
+    # _top_num = 10
+    # print(f"\nTop {_top_num} elements:")
+    # _tops = find_top_n(_tuples, n=_top_num, top_max=True)
+    # pprint(_tops)
 
-    _top_num = 10
-    print(f"\nTop {_top_num} elements:")
-    _tops = find_top_n(_tuples, n=_top_num, top_max=True)
-    pprint(_tops)
+    # Find the Average Similarities inside a group of vector.
+    _vectors = [
+        np.array([1.0, 2.0, 3.0, 4.0]),
+        np.array([2.0, 2.0, 3.0, 4.0]),
+        np.array([3.0, 4.0, 3.0, 4.0]),
+        np.array([4.0, 5.0, 6.0, 7.0]),
+        np.array([9.0, 8.0, 7.0, 6.0]),
+        np.array([5.0, 9.0, 2.0, 1.0]),
+    ]
+    _average_sim = mean_similarity(vectors_ndarray=_vectors)
+    print(f"\nMean Similarity of the Vectors: {_average_sim}")
 
     print("\nDone.")
     print(f"[{_stopwatch.formatted_runtime()}]\n")
